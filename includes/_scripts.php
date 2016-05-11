5f70805027ba7d44029f7dd411aadc38f4b1ddce
Son Chau <sonchau24@yahoo.com>
2016-05-11 14:42:28 +1000

    Son: fix links to registration form

--- includes/_scripts.php
+++ includes/_scripts.php
@@ -1,9 +1,8 @@
 ﻿    <!-- <link href="/css/bootstrap.css" rel="stylesheet" id="css"/> -->
-    <link href="/css/cosmo.css" rel="stylesheet" id="css"/>
-    <link href="/css/Site.css" rel="stylesheet" />
-    <link href="/css/font-awesome.css" rel="stylesheet" type="text/css" />
-    <link href="/css/checkbox.css" rel="stylesheet" type="text/css" />
-    <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <link href="/css/v2/cosmo.css" rel="stylesheet" id="css"/>
+    <link href="/css/site.css" rel="stylesheet" />
+    <link href="/css/v2/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="/css/v2/checkbox.css" rel="stylesheet" type="text/css" />
     <script src="//code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
     <script src="/js/bootstrap.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.2/modernizr.min.js"></script>
@@ -14,7 +13,7 @@
     <![endif]-->
     <script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
     <script src="/js/intlTelInput.min.js"></script>
-    <link href="/css/intlTelInput.css" rel="stylesheet" />
-    <link href="/css/register.css?v39111" rel="stylesheet" />
-    <link href="/css/print.css?v1" rel="stylesheet" media="print" />
-    <link href="/css/panels.css" rel="stylesheet" media="print" />
+    <link href="/css/v2/intlTelInput.css" rel="stylesheet" />
+    <link href="/css/v2/register.css?v39111" rel="stylesheet" />
+    <link href="/css/v2/print.css?v1" rel="stylesheet" media="print" />
+    <link href="/css/v2/panels.css" rel="stylesheet" media="print" />
